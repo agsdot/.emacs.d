a21e6d4746d818952e3a6df53e16d5f62868ef79
Jonathan Lai <jonathanlai@eltux.net>
2014-12-05 21:14:06 -0800

    progn and indentation

--- init.el
+++ init.el
@@ -34,8 +34,9 @@
 (use-package f
   :ensure f
   :init
-  (unless (f-exists? dotemacs-cache-directory)
-    (f-mkdir dotemacs-cache-directory)))
+  (progn
+    (unless (f-exists? dotemacs-cache-directory)
+      (f-mkdir dotemacs-cache-directory))))
 
 ;; Dont display logo at startup
 (setq inhibit-startup-message t)
@@ -67,17 +68,18 @@
 (use-package undo-tree
   :ensure undo-tree
   :config
-    (global-undo-tree-mode t))
+  (progn
+    (global-undo-tree-mode t)))
 
 (use-package cross-platform-copy-paste)
 
 (use-package recentf
   :ensure recentf
   :config
-    (progn
-      (recentf-mode 1)
-      (setq recentf-max-menu-items 25)
-      (setq recentf-save-file (concat dotemacs-cache-directory "recentf"))))
+  (progn
+    (recentf-mode 1)
+    (setq recentf-max-menu-items 25)
+    (setq recentf-save-file (concat dotemacs-cache-directory "recentf"))))
 
 (setq-default tab-width 2)
 (setq-default indent-tabs-mode nil) ;; seems to affect autocomplete modes
@@ -90,9 +92,9 @@
 (use-package smartparens
   :ensure t
   :config
-    (progn
-      (require 'smartparens-config)
-      (smartparens-global-mode 1)))
+  (progn
+    (require 'smartparens-config)
+    (smartparens-global-mode 1)))
 
 (line-number-mode t)
 (column-number-mode t)
@@ -118,18 +120,20 @@
 (use-package smex
   :ensure smex
   :config
-    (progn
-      (smex-initialize)
-      (setq smex-save-file (concat dotemacs-cache-directory "smex-items"))
-      (global-set-key (kbd "M-x") 'smex)
-      (global-set-key (kbd "M-X") 'smex-major-mode-commands)
-      ;; This is your old M-x.
-      (global-set-key (kbd "C-c C-c M-x") 'execute-extended-command)))
+  (progn
+    (smex-initialize)
+    (setq smex-save-file (concat dotemacs-cache-directory "smex-items"))
+    (global-set-key (kbd "M-x") 'smex)
+    (global-set-key (kbd "M-X") 'smex-major-mode-commands)
+    ;; This is your old M-x.
+    (global-set-key (kbd "C-c C-c M-x") 'execute-extended-command)))
 
 ;; https://github.com/krobertson/emacs.d/blob/master/packages.el
 (use-package projectile
   :ensure projectile
-  :init (projectile-global-mode 1)
+  :init
+  (progn
+    (projectile-global-mode 1))
   :config
   (progn
     (setq projectile-known-projects-file (concat dotemacs-cache-directory "projectile-bookmarks.eld"))
@@ -138,24 +142,27 @@
 (use-package ace-jump-mode
   :ensure ace-jump-mode
   :config
-    (progn
-      (define-key global-map (kbd "C-c SPC") 'ace-jump-mode)))
+  (progn
+    (define-key global-map (kbd "C-c SPC") 'ace-jump-mode)))
 
 (use-package menu-bar
   :config
-  (menu-bar-mode -1))
+  (progn
+    (menu-bar-mode -1)))
 
 ;; ido mode configs
 (use-package ido-vertical-mode
   :ensure ido-vertical-mode
   :init
-  (ido-vertical-mode t))
+  (progn
+    (ido-vertical-mode t)))
 
 ;; https://github.com/bdd/.emacs.d/blob/master/packages.el
 (use-package flx-ido
   :ensure flx-ido
   :init
-  (flx-ido-mode 1)
+  (progn
+    (flx-ido-mode 1))
   :config
   (progn
     (setq flx-ido-threshhold 1000)
@@ -164,17 +171,16 @@
 (use-package saveplace
   :ensure saveplace
   :init
-  (setq-default save-place t)
+  (progn
+    (setq-default save-place t))
   :config
-  (setq save-place-forget-unreadable-files nil)
-  ;; Try to make emacsclient play nice with saveplace
-  ;; http://www.emacswiki.org/emacs/EmacsClient#toc35
-  (setq server-visit-hook (quote (save-place-find-file-hook)))
-  ;; rename this save file....
-  (setq save-place-file "~/.emacs.d/.cache/saved-places"))
-  ;; (setq save-place-file (concat dotemacs-cache-directory "saved-places"))
-  ;; (setq smex-save-file (concat dotemacs-cache-directory "smex-items"))
-  ;; (setq save-place-file (concat dotemacs-cache-directory "saved-places"))
+  (progn
+    (setq save-place-forget-unreadable-files nil)
+    ;; Try to make emacsclient play nice with saveplace
+    ;; http://www.emacswiki.org/emacs/EmacsClient#toc35
+    (setq server-visit-hook (quote (save-place-find-file-hook)))
+    ;; rename this save file....
+    (setq save-place-file "~/.emacs.d/.cache/saved-places")))
 
 ;; coding ;;
 
@@ -185,7 +191,8 @@
   :ensure git-gutter
   :diminish git-gutter-mode
   :init
-  (global-git-gutter-mode 1))
+  (progn
+    (global-git-gutter-mode 1)))
 
 (use-package git-timemachine
   :ensure git-timemachine)
@@ -193,80 +200,85 @@
 (use-package magit
   :ensure magit
   :init
-    (progn
-      ;; http://whattheemacsd.com/setup-magit.el-01.html
-      (defadvice magit-status (around magit-fullscreen activate)
-        (window-configuration-to-register :magit-fullscreen)
-        ad-do-it
-        (delete-other-windows))
-      (defun magit-quit-session ()
-        "Restores the previous window configuration and kills the magit buffer"
-        (interactive)
-        (kill-buffer)
-        (jump-to-register :magit-fullscreen))))
+  (progn
+    ;; http://whattheemacsd.com/setup-magit.el-01.html
+    (defadvice magit-status (around magit-fullscreen activate)
+      (window-configuration-to-register :magit-fullscreen)
+      ad-do-it
+      (delete-other-windows))
+    (defun magit-quit-session ()
+      "Restores the previous window configuration and kills the magit buffer"
+      (interactive)
+      (kill-buffer)
+      (jump-to-register :magit-fullscreen))))
 
 (use-package web-mode
   :ensure web-mode
   :config
-    (progn
-      (add-to-list 'auto-mode-alist '("\\.html?\\'" . web-mode))
-      (add-to-list 'auto-mode-alist '("\\.gsp?\\'" . web-mode))))
+  (progn
+    (add-to-list 'auto-mode-alist '("\\.html?\\'" . web-mode))
+    (add-to-list 'auto-mode-alist '("\\.gsp?\\'" . web-mode))))
 
 (use-package js2-mode
   :ensure js2-mode
   :config
-    (progn
-      (add-to-list 'auto-mode-alist '("\\.js?\\'" . js2-mode))))
+  (progn
+    (add-to-list 'auto-mode-alist '("\\.js?\\'" . js2-mode))))
 
 (use-package groovy-mode
   :ensure groovy-mode
   :config
-    (progn
-      (autoload 'groovy-mode "groovy-mode" "Major mode for editing Groovy code." t)
-      (add-to-list 'auto-mode-alist '("\.groovy$" . groovy-mode))
-      (add-to-list 'auto-mode-alist '("\.gradle$" . groovy-mode))
-      (add-to-list 'interpreter-mode-alist '("groovy" . groovy-mode))))
+  (progn
+    (autoload 'groovy-mode "groovy-mode" "Major mode for editing Groovy code." t)
+    (add-to-list 'auto-mode-alist '("\.groovy$" . groovy-mode))
+    (add-to-list 'auto-mode-alist '("\.gradle$" . groovy-mode))
+    (add-to-list 'interpreter-mode-alist '("groovy" . groovy-mode))))
 
 (use-package lua-mode
   :ensure lua-mode
   :config
-    (progn
-      (add-to-list 'auto-mode-alist '("\\.lua?\\'" . js2-mode))))
+  (progn
+    (add-to-list 'auto-mode-alist '("\\.lua?\\'" . js2-mode))))
 
 (use-package vimrc-mode
   :ensure vimrc-mode
   :config
-    (progn
-      (add-to-list 'auto-mode-alist '(".vim\\(rc\\)?$" . vimrc-mode))))
+  (progn
+    (add-to-list 'auto-mode-alist '(".vim\\(rc\\)?$" . vimrc-mode))))
 
 (use-package drag-stuff
   :ensure drag-stuff
   :config
-    (progn
-      (drag-stuff-global-mode t)))
+  (progn
+    (drag-stuff-global-mode t)))
 
 ;; http://stackoverflow.com/a/15310340/2741455
 ;; How to set defcustom variable
 (use-package linum-relative
   :ensure linum-relative
   :init
+  (progn
     (setq linum-relative-format "%3s ")
-    (setq linum-relative-current-symbol ""))
+    (setq linum-relative-current-symbol "")))
 
 (cond ((executable-find "pt")
-        (use-package pt
-          :ensure pt) ;; https://github.com/bling/pt.el
-        (defalias 'my-search-util 'projectile-pt))  ;; seems pretty fast (faster than ag? maybe...dunno), but it's written in Go!
+        (progn
+          (use-package pt
+            :ensure pt) ;; https://github.com/bling/pt.el
+          (defalias 'my-search-util 'projectile-pt)))  ;; seems pretty fast (faster than ag? maybe...dunno), but it's written in Go!
       ((executable-find "ag")
-        (use-package ag
-          :ensure ag) ;; https://github.com/Wilfred/ag.el
-        (defalias 'my-search-util 'projectile-ag))  ;; on the website, it said faster than ack
+        (progn
+          (use-package ag
+            :ensure ag) ;; https://github.com/Wilfred/ag.el
+          (defalias 'my-search-util 'projectile-ag)))  ;; on the website, it said faster than ack
       ((executable-find "ack")
-        (use-package ack-and-a-half
-          :ensure ack) ;; https://github.com/jhelwig/ack-and-a-half
-        (defalias 'my-search-util 'projectile-ack))  ;; faster than grep
+        (progn
+          (use-package ack-and-a-half
+            :ensure ack) ;; https://github.com/jhelwig/ack-and-a-half
+          (defalias 'my-search-util 'projectile-ack)))  ;; faster than grep
       ((executable-find "grep")
-        (defalias 'my-search-util 'projectile-grep)))
+        (progn
+          (defalias 'my-search-util 'projectile-grep))))
 
 ;; evil mode setup ;;;
 (setq evil-want-C-u-scroll t)
@@ -325,6 +337,7 @@
         (global-evil-leader-mode t)
         (evil-leader/set-leader ",")
         (evil-leader/set-key
+          "a" 'ace-jump-mode
           "b" 'buffer-menu
           "f" 'my-search-util
           "l" 'linum-relative-toggle
@@ -340,8 +353,8 @@
       :ensure evil-nerd-commenter
       :commands (evilnc-comment-or-uncomment-lines)
       :config
-        (progn
-          (evilnc-default-hotkeys)))
+      (progn
+        (evilnc-default-hotkeys)))
 
     (use-package evil-matchit
       :ensure evil-matchit
@@ -358,24 +371,24 @@
     (use-package evil-jumper
       :ensure evil-jumper
       :config
-        (progn
-          (setq evil-jumper-auto-center t)
-          (setq evil-jumper-file (concat dotemacs-cache-directory "evil-jumps"))
-          (setq evil-jumper-auto-save-interval 120)
-          (setq evil-jumper-max-length 10)))
+      (progn
+        (setq evil-jumper-auto-center t)
+        (setq evil-jumper-file (concat dotemacs-cache-directory "evil-jumps"))
+        (setq evil-jumper-auto-save-interval 120)
+        (setq evil-jumper-max-length 10)))
 
     (use-package evil-numbers
       :ensure evil-numbers
       :config
-        (progn
-          (define-key evil-normal-state-map (kbd "C-<right>") 'evil-numbers/inc-at-pt)
-          (define-key evil-normal-state-map (kbd "C-<left>") 'evil-numbers/dec-at-pt)))
+      (progn
+        (define-key evil-normal-state-map (kbd "C-<right>") 'evil-numbers/inc-at-pt)
+        (define-key evil-normal-state-map (kbd "C-<left>") 'evil-numbers/dec-at-pt)))
 
     (use-package powerline-evil
       :ensure powerline-evil
       :config
-        (progn
-          (powerline-evil-vim-theme)))
+      (progn
+        (powerline-evil-vim-theme)))
 
     (use-package key-chord
       :ensure key-chord
