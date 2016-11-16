91cf90683a494eafaf87a20c2108f6c88446d4cb
Jonathan Lai <jonathanlai@gmail.com>
2016-11-16 12:29:00 -0800

    Polish up configs

--- init.el
+++ init.el
@@ -1,2 +1,4 @@
+;; https://www.reddit.com/r/emacs/comments/56fvgd/is_there_a_way_to_stop_emacs_from_adding_the/
+(setq package--init-file-ensured t)
 ;; https://labo.olivierdelort.net/bricewge/dotfiles-emacs/blob/9c32a1232708137f7e247c8ceef79826ff9ff933/.emacs.d/init.el
 (org-babel-load-file (expand-file-name "emacs-init.org" user-emacs-directory))
