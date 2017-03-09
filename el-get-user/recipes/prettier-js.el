69b528f671033384d5e35ba3b1102a772db6a7ff
Jonathan Lai <jonathanlai@gmail.com>
2017-03-09 00:52:12 -0800

    Add restart-emacs, prettier-js, and reorganize lines of code

---
+++ el-get-user/recipes/prettier-js.el
@@ -0,0 +1,5 @@
+(:name prettier-js
+       :description "Utility functions to format reason code"
+       :type http
+       :url "https://raw.githubusercontent.com/prettier/prettier/master/editors/emacs/prettier-js.el"
+       :features "prettier-js")
