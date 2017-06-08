0d5349c60a22bc02a95867f6df148665980e9a51
Jonathan Lai <jonathanlai@gmail.com>
2017-06-08 11:08:13 -0700

    Macro for el-get-bundle and use-package

--- el-get-user/recipes/prettier-js.el
+++ el-get-user/recipes/prettier-js.el
@@ -1,5 +1,5 @@
 (:name prettier-js
-       :description "Utility functions to format reason code"
-       :type http
-       :url "https://raw.githubusercontent.com/prettier/prettier/master/editors/emacs/prettier-js.el"
-       :features "prettier-js")
+       :website "https://github.com/prettier/prettier-emacs"
+       :description "Minor mode to format JS code on file save "
+       :type github
+       :pkgname "prettier/prettier-emacs")
