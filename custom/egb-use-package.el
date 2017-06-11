d1c5d314e4af92736e889f63f6c0310647ad0d05
Jonathan Lai <jonathanlai@gmail.com>
2017-06-11 00:28:56 -0700

    variables function in egb5 now

--- custom/egb-use-package.el
+++ custom/egb-use-package.el
@@ -2,72 +2,56 @@
 ;; basing off initial commit of quse-package
 ;; https://github.com/jaccarmac/quse-package/commit/fd7316facec783db0c5c202d45c7350e43fe6821?diff=unified
 
-;;;###autoload
-(defmacro egb-use-package (el-get-form &rest use-package-forms)
-  "Download a package with el-get-bundle and initialize it with use-package.
-   el-get-form should be a name compatible with el-get recipes.
-   use-package-form should be whatever comes after the package name in a use-package call."
-  (let ((use-package-name (if (listp el-get-form)
-                              (car el-get-form)
-                            el-get-form)))
-    `(progn (el-get-bundle ,el-get-form)
-            (use-package ,use-package-name
-              ,@use-package-forms))))
-
 
 ;;;###autoload
 (defmacro egb-use-package2 (el-get-form &rest use-package-forms)
-  (let* ((el-get-features (if (and use-package-forms
-                                (eq :features (car use-package-forms)))
+  (let* (
+
+	 (el-get-features (if (and use-package-forms (eq :features (car use-package-forms)))
                            (list (car use-package-forms)
                                  (cadr use-package-forms))))
+
          (use-package-name el-get-form)
+
+         (stuffel-get-features-present (and use-package-forms (eq :features (car use-package-forms)) ) )
+
          (use-package-forms (if el-get-features
                                 (cddr use-package-forms)
-                              use-package-forms)))
+                              use-package-forms))
+	 )
+
     `(progn (el-get-bundle ,el-get-form ,@el-get-features)
             (use-package ,use-package-name
               ,@use-package-forms))))
 
-;; next step, provide way for the el-get-bundle name and use-package to be named differently
-(defmacro egb-use-package3 (el-get-form &optional upn &rest use-package-forms)
-  (let* ((el-get-features (if (and use-package-forms
-                                (eq :features (car use-package-forms)))
-                           (list (car use-package-forms)
-                                 (cadr use-package-forms))))
-         (use-package-name (if (upn)
-			     upn
-			     el-get-form))
-         (use-package-forms (if el-get-features
-                                (cddr use-package-forms)
-                              use-package-forms)))
-    `(progn (el-get-bundle ,el-get-form ,@el-get-features)
-            (use-package ,use-package-name
-              ,@use-package-forms))))
 
+;;;###autoload
 (defmacro egb-use-package5 (el-get-form &rest use-package-forms)
+  ;; https://emacs.stackexchange.com/questions/25026/symbols-function-definition-is-void-on-built-in-variables
+  ;; You get Symbol's function definition is void because those are variables, not functions.
 
-(setq el-get-features-present (and use-package-forms (eq :features (car use-package-forms))))
+( setq el-get-features-present2 (and use-package-forms (eq :features (car use-package-forms)) ) )
+;;( el-get-features-present (if  use-package-forms (eq :features (car use-package-forms)) ) )
 
-(setq pkgname-present (if (el-get-features-present)
-       	      (eq :pkgname (car (cddr use-package-forms)))
-       	    (and use-package-forms (eq :pkgname (car use-package-forms)))))
+( setq pkgname-present2 (if el-get-features-present2
+   	      (eq :pkgname (car (cddr use-package-forms)))
+   	    (and use-package-forms (eq :pkgname (car use-package-forms)))))
 
-(setq el-get-features (if (el-get-features-present)
-                     (list (car use-package-forms) (cadr use-package-forms))))
+( setq el-get-features (if el-get-features-present2
+                 (list (car use-package-forms) (cadr use-package-forms))))
 
 
-(setq pkgname         (if (pkgname-present)
-       	      (if el-get-features-present
-                         (nth 3 use-package-forms)
-       		(nth 1 use-package-forms))
-       	    el-get-form))
+( setq pkgname         (if pkgname-present2
+   	      (if el-get-features-present2
+                     (nth 3 use-package-forms)
+   		(nth 1 use-package-forms))
+   	    el-get-form))
 
-(setq use-package-forms  (if el-get-features-present
-			     (if pkgname-present
+( setq use-package-forms  (if el-get-features-present2
+			     (if pkgname-present2
 			         (nthcdr 4 use-package-forms)
 	                       (nthcdr 2 use-package-forms))
-                           (if pkgname-present
+                           (if pkgname-present2
 			       (nthcdr 2 use-package-forms)
 			       use-package-forms
 			   )))
@@ -77,60 +61,6 @@
             (use-package ,pkgname
               ,@use-package-forms)))
 
-
-
-
-
-
-
-(defmacro egb-use-package6 (el-get-form &rest use-package-forms)
-
-;;(setq el-get-features-present 
-;;(and use-package-forms (eq :features (car use-package-forms)))
-;;      )
-;;
-;;(setq pkgname-present 
-;;      
-;;(if (and use-package-forms (eq :features (car use-package-forms))) (eq :pkgname (car (cddr use-package-forms))) (and use-package-forms (eq :pkgname (car use-package-forms))))
-;;
-;;)
-
-
-(setq el-get-features (if (and use-package-forms (eq :features (car use-package-forms)))
-                     (list (car use-package-forms) (cadr use-package-forms))))
-
-
-(setq pkgname         (if (if (and use-package-forms (eq :features (car use-package-forms))) (eq :pkgname (car (cddr use-package-forms))) (and use-package-forms (eq :pkgname (car use-package-forms))))
-
-       	      (if (and use-package-forms (eq :features (car use-package-forms)))
-                         (nth 3 use-package-forms)
-       		(nth 1 use-package-forms))
-       	    el-get-form))
-
-;;(setq use-package-forms  (if el-get-features-present
-;;			     (if pkgname-present
-;;			         (nthcdr 4 use-package-forms) 
-;;	                       (nthcdr 2 use-package-forms))
-;;                           (if pkgname-present
-;;			       (nthcdr 2 use-package-forms)
-;;			       use-package-forms
-;;			   )))
-
-(setq use-package-forms  (if (and use-package-forms (eq :features (car use-package-forms)))
-			     (if (if (and use-package-forms (eq :features (car use-package-forms))) (eq :pkgname (car (cddr use-package-forms))) (and use-package-forms (eq :pkgname (car use-package-forms))))
-
-			         (nthcdr 4 use-package-forms) 
-	                       (nthcdr 2 use-package-forms))
-                           (if (if (and use-package-forms (eq :features (car use-package-forms))) (eq :pkgname (car (cddr use-package-forms))) (and use-package-forms (eq :pkgname (car use-package-forms))))
-
-			       (nthcdr 2 use-package-forms)
-			       use-package-forms
-			   )))
-
-
-    `(progn (el-get-bundle ,el-get-form ,@el-get-features)
-            (use-package ,pkgname
-              ,@use-package-forms)))
 
 
 (provide 'egb-use-package)
